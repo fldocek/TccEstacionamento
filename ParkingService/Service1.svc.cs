be7bf0eb3d4bc8d9fe065e19dfaa50bbda85963a
fldocek <fldocek@gmail.com>
2015-05-22 20:54:06 -0300

    teste

--- ParkingService/Service1.svc.cs
+++ ParkingService/Service1.svc.cs
@@ -19,6 +19,7 @@ namespace ParkingService
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
+
             if (composite == null)
             {
                 throw new ArgumentNullException("composite");
