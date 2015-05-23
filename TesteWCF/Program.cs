ca8dc559fd471b2f18e6ab9ae2a97f49cadfde77
fldocek <fldocek@gmail.com>
2015-05-23 00:18:23 -0300

    Projeto de teste

---
+++ TesteWCF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servico = new com.somee.smartparking.Service1();
+
+            string teste = servico.GetData(10, true);
+
+            Console.WriteLine("Teste: " + teste);
+
+            Console.ReadLine();
+        }
+    }
+}
