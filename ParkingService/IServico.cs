e1ac5f28c4edeed244229011b03e62d2112ac9a7
fldocek <fldocek@gmail.com>
2015-07-20 21:43:26 -0300

    json

--- ParkingService/IServico.cs
+++ ParkingService/IServico.cs
@@ -17,30 +17,51 @@ namespace ParkingService
         //===================
 
         [OperationContract]
+        [WebInvoke(Method=  "GET",
+            ResponseFormat= WebMessageFormat.Json,
+            BodyStyle= WebMessageBodyStyle.WrappedRequest)]
         IEnumerable<dtoAndar> ListarAndares();
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         IEnumerable<dtoBloco> ListarBlocos(int Id_Andar);
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         IEnumerable<dtoVaga> ListarVagas(int Id_Bloco);
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         bool ReservarVaga(int Id_Vaga, int Id_Carro);
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         IEnumerable<dtoCarro> ListarCarros(string CPF);
 
         //Fluxo Manter Vaga Reservada
         //===========================
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         dtoSituacaoVaga ConsultaSituacaoVaga(int Id_Vaga, int Id_Carro);
 
         //Fluxo Localizar Carro
         //=====================
 
         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         dtoCaminho LocalizarCarro(int Id_QRCode, int Id_Carro);
 
     }
