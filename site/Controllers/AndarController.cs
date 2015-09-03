d56ac7ed8e32cef4a0188143bdbfb71556c0877c
fldocek <fldocek@gmail.com>
2015-09-02 23:02:57 -0300

    layout

---
+++ site/Controllers/AndarController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dados;
+
+namespace site.Controllers
+{
+    public class AndarController : Controller
+    {
+        private ParkingDBEntities db = new ParkingDBEntities();
+
+        //
+        // GET: /Andar/
+
+        public ActionResult Index()
+        {
+            return View(db.Andar.ToList());
+        }
+
+        //
+        // GET: /Andar/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Andar andar = db.Andar.Find(id);
+            if (andar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(andar);
+        }
+
+        //
+        // GET: /Andar/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Andar/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Andar andar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Andar.Add(andar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(andar);
+        }
+
+        //
+        // GET: /Andar/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Andar andar = db.Andar.Find(id);
+            if (andar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(andar);
+        }
+
+        //
+        // POST: /Andar/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Andar andar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(andar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(andar);
+        }
+
+        //
+        // GET: /Andar/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Andar andar = db.Andar.Find(id);
+            if (andar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(andar);
+        }
+
+        //
+        // POST: /Andar/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Andar andar = db.Andar.Find(id);
+            db.Andar.Remove(andar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ site/Controllers/BlocoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dados;
+
+namespace site.Controllers
+{
+    public class BlocoController : Controller
+    {
+        private ParkingDBEntities db = new ParkingDBEntities();
+
+        //
+        // GET: /Bloco/
+
+        public ActionResult Index()
+        {
+            var bloco = db.Bloco.Include(b => b.Andar);
+            return View(bloco.ToList());
+        }
+
+        //
+        // GET: /Bloco/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Bloco bloco = db.Bloco.Find(id);
+            if (bloco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bloco);
+        }
+
+        //
+        // GET: /Bloco/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Id_Andar = new SelectList(db.Andar, "Id", "Nome");
+            return View();
+        }
+
+        //
+        // POST: /Bloco/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Bloco bloco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bloco.Add(bloco);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_Andar = new SelectList(db.Andar, "Id", "Nome", bloco.Id_Andar);
+            return View(bloco);
+        }
+
+        //
+        // GET: /Bloco/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Bloco bloco = db.Bloco.Find(id);
+            if (bloco == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_Andar = new SelectList(db.Andar, "Id", "Nome", bloco.Id_Andar);
+            return View(bloco);
+        }
+
+        //
+        // POST: /Bloco/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Bloco bloco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bloco).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_Andar = new SelectList(db.Andar, "Id", "Nome", bloco.Id_Andar);
+            return View(bloco);
+        }
+
+        //
+        // GET: /Bloco/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Bloco bloco = db.Bloco.Find(id);
+            if (bloco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bloco);
+        }
+
+        //
+        // POST: /Bloco/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bloco bloco = db.Bloco.Find(id);
+            db.Bloco.Remove(bloco);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
